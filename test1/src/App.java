7deb35faabecf3f4aebc747e5a70204e3e833af8
entroyd <shrkdals0803@daum.net>
2024-03-22 16:53:31 +0900

    수업 끝

--- test1/src/App.java
+++ test1/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.print("Hello, World! \n");
-        
+        System.out.println("hello world "+ 13 + 1.43 + "!");
+        System.out.println( 13 + 1.43 + "!");
     }
 }
