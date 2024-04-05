f60736453bc8cb3d889d3d814b720e50fcb900e6
entroyd <shrkdals0803@daum.net>
2024-04-05 14:31:54 +0900

    나뉨수

--- test1/src/study.java
+++ test1/src/study.java
@@ -3,7 +3,7 @@ int a =0;
 int b =0;
 int c =0;
 public class study {
-    public static int sum(int a ,int b, int c){
+    public static int sum(int a ,int b){
         
         if (a>200){
             System.out.println("첫번쨰수가 200보다 크면 작동 하지 않습니다");
@@ -15,7 +15,6 @@ public class study {
             
             
         }
-        return c;
         
         
     }
@@ -28,13 +27,11 @@ public class study {
         int a = scanner.nextInt();
         System.out.print("두번쨰수 :");
         int b = scanner.nextInt();
-        System.out.print("세번쨰 수 : ");
-        int c = scanner.nextInt();
         if(a<200){
             
             
         }else{
-        System.out.println(sum(a,b,c)+"입니다");
+        System.out.println(sum(a,b)+"입니다");
         }
         scanner.close();
 
