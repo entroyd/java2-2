09492fbf699f3ff2051a4dceeb358e9e648e0314
entroyd <shrkdals0803@daum.net>
2024-04-12 17:17:42 +0900

    gyg

--- test1/src/SuperEx.java
+++ test1/src/SuperEx.java
@@ -1,32 +1,32 @@
-    
+
 class Point {
     private int x, y; 
     public Point() {
-    this.x= this.y = 8;
+        this.x = this.y = 8;
     }
     public Point(int x, int y) {
-    this.x = x; this.y = y;
-}
+      this.x = x; this.y = y;
+    }
     public void showPoint() {
-    System.out.println("(" + x+"," + y + ")");
+       System.out.println("(" + x+"," + y + ")");
     }
 }
-    class ColorPoint extends Point{
+class ColorPoint extends Point{
     private String color; 
     public ColorPoint(int x, int y, String color){
-    super(x, y);
-    this.color = color;
-}
+        super(x, y);
+        this.color = color;
+    }
     public void showColorPoint(){
-    System.out.print(color);
-    showPoint(); 
+        System.out.print(color);
+        showPoint(); 
     
     }
 }
 
-    public class SuperEx {
-    public static void main(String[] args){
-    ColorPoint cp = new ColorPoint(5, 6, "blue");
-    cp.showColorPoint();
+public class SuperEx {
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint(5, 6, "blue");
+        cp.showColorPoint();
     }
 }
